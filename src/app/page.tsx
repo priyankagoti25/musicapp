d2ed00b475accc2ddc1ceab775711ca9d77005a7
priyankagoti25 <goti.priyanka25@gmail.com>
2025-05-16 18:45:20 +0530

    Designed testimonials and why choose us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import TestimonialCards from "@/components/TestimonialCards";
 
 export default function Home() {
   return (
       <main className="min-h-screen bg-grid-white/[0.02]">
           <HeroSection/>
           <FeaturedCourses/>
+          <WhyChooseUs/>
+          <TestimonialCards/>
       </main>
 
   );
